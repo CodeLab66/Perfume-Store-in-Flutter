aaf03a3817fcb624d793f30f3dc5a3a00b7de0b9
Muhammad Bilal <bscs23146@itu.edu.pk>
2025-03-06 18:57:09 +0500

    Created Splash Screen for the app
    I have created a splash screen for the app. The splash screen is displayed for 5 seconds and then the app navigates to the started screen. The splash screen is displayed while the app is loading and provides a visual indication to the user that the app is starting up.

---
+++ perfume_ecommerce_store/lib/screens/get_started_screen.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class GetStartedScreen extends StatelessWidget {
+  const GetStartedScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Get Started')),
+      body: const Center(child: Text('Welcome to ROSELE')),
+    );
+  }
+}
---
+++ perfume_ecommerce_store/lib/screens/splash_screen.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+import 'dart:async';
+import 'get_started_screen.dart';
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({super.key});
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState() {
+    super.initState();
+    Timer(Duration(seconds: 5), () {
+      Navigator.pushReplacement(
+        context,
+        MaterialPageRoute(builder: (context) => GetStartedScreen()),
+      );
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: ClipRRect(
+          borderRadius: BorderRadius.circular(20),
+          child: Image.asset(
+            'assets/images/roselle.png',
+            width: 150,
+            height: 150,
+          ),
+        ),
+      ),
+    );
+  }
+}

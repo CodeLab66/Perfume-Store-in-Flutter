f1fd5ac26d8f31cc6158c864709ce19661ee9779
Muhammad Bilal <bscs23146@itu.edu.pk>
2025-03-22 14:31:22 +0500

    Reduce splash screen timer duration and enhance Get Started screen layout with new assets and improved UI elements.

--- perfume_ecommerce_store/lib/screens/get_started_screen.dart
+++ perfume_ecommerce_store/lib/screens/get_started_screen.dart
@@ -6,8 +6,70 @@ class GetStartedScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text('Get Started')),
-      body: const Center(child: Text('Welcome to ROSELE')),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Padding(
+            padding: const EdgeInsets.only(top: 5.0),
+            child: ClipRRect(
+              borderRadius: BorderRadius.circular(10),
+              child: Image.asset(
+                'assets/images/roselle.png',
+                width: MediaQuery.of(context).size.width * 0.15,
+                fit: BoxFit.contain,
+              ),
+            ),
+          ),
+          const SizedBox(height: 10),
+          const Text(
+            'Rosele',
+            style: TextStyle(fontSize: 28, fontFamily: 'AnticSlab'),
+          ),
+          const SizedBox(height: 20),
+          Image.asset(
+            'assets/images/getStarted.png',
+            height: 400,
+            fit: BoxFit.cover,
+          ),
+          const SizedBox(height: 20),
+          const Padding(
+            padding: EdgeInsets.symmetric(horizontal: 20.0),
+            child: Text(
+              'Experience the Essence of\nLuxury Perfumes',
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                fontSize: 19,
+                fontFamily: 'AnticSlab',
+                color: Colors.black,
+              ),
+            ),
+          ),
+          const SizedBox(height: 30),
+          ElevatedButton(
+            onPressed: () {
+              // Add navigation or functionality here
+            },
+            style: ElevatedButton.styleFrom(
+              backgroundColor: const Color(0xFFE8A0A0),
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.only(
+                  bottomLeft: Radius.circular(30),
+                  topRight: Radius.circular(30),
+                ),
+              ),
+              padding: const EdgeInsets.symmetric(horizontal: 50, vertical: 10),
+            ),
+            child: const Text(
+              'Get Started',
+              style: TextStyle(
+                fontSize: 19,
+                fontFamily: 'AnticSlab',
+                color: Colors.white,
+              ),
+            ),
+          ),
+        ],
+      ),
     );
   }
 }

ed4111c3a5f686b1f35d1cd6c6139b71f98a56c4
Muhammad Bilal <codelab.bilal@gmail.com>
2025-05-11 12:42:14 +0500

    Refactor FirestoreService to include Product model and implement product fetching logic

--- perfume_ecommerce_store/lib/firebase/index_firebase.dart
+++ perfume_ecommerce_store/lib/firebase/index_firebase.dart
@@ -1,15 +1,13 @@
-// lib/firebase/index_firebase.dart
-
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'product_firebase.dart';
 
 class FirestoreService {
   final FirebaseFirestore _db = FirebaseFirestore.instance;
 
-  /// Fetch products from Firestore based on category (arrayContains!)
   Stream<List<Product>> fetchProductsByCategory(String category) {
     return _db
         .collection('products')
-        .where('category', arrayContains: category) // ← use arrayContains
+        .where('category', arrayContains: category)
         .snapshots()
         .map(
           (snap) =>
@@ -17,37 +15,5 @@ class FirestoreService {
                   .map((doc) => Product.fromFirestore(doc.data()))
                   .toList(),
         );
-  }
-}
-
-class Product {
-  final String name;
-  final String description;
-  final String image;
-  final double price;
-  final String size;
-
-  Product({
-    required this.name,
-    required this.description,
-    required this.image,
-    required this.price,
-    required this.size,
-  });
-
-  factory Product.fromFirestore(Map<String, dynamic> data) {
-    final rawImage = data['image'] as String? ?? '';
-    return Product(
-      name: data['name'] as String? ?? '',
-      description: data['description'] as String? ?? '',
-      image: _formatGoogleDriveImageURL(rawImage),
-      price: (data['price'] as num?)?.toDouble() ?? 0.0,
-      size: data['size'] as String? ?? '',
-    );
-  }
-
-  static String _formatGoogleDriveImageURL(String urlOrId) {
-    if (urlOrId.startsWith('http')) return urlOrId;
-    return 'https://drive.google.com/uc?id=$urlOrId';
   }
 }
---
+++ perfume_ecommerce_store/lib/firebase/product_firebase.dart
@@ -0,0 +1,31 @@
+class Product {
+  final String name;
+  final String description;
+  final String image;
+  final double price;
+  final String size;
+
+  Product({
+    required this.name,
+    required this.description,
+    required this.image,
+    required this.price,
+    required this.size,
+  });
+
+  factory Product.fromFirestore(Map<String, dynamic> data) {
+    final rawImage = data['image'] as String? ?? '';
+    return Product(
+      name: data['name'] as String? ?? '',
+      description: data['description'] as String? ?? '',
+      image: _formatGoogleDriveImageURL(rawImage),
+      price: (data['price'] as num?)?.toDouble() ?? 0.0,
+      size: data['size'] as String? ?? '',
+    );
+  }
+
+  static String _formatGoogleDriveImageURL(String urlOrId) {
+    if (urlOrId.startsWith('http')) return urlOrId;
+    return 'https://drive.google.com/uc?id=$urlOrId';
+  }
+}
